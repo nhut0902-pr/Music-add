2dec20c0447a4c98066374b5e7714fd24c9f166d
nhut0902-pr <lamminhnhut09022011@gmail.com>
2025-07-10 10:34:55 +0700

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,12 +1,24 @@
 // netlify/functions/login.js
 const querystring = require('querystring');
 
-const {
-  SPOTIFY_CLIENT_ID,
-  SPOTIFY_REDIRECT_URI_NETLIFY
-} = process.env;
-
 exports.handler = async (event, context) => {
+  // In tất cả các biến môi trường ra log để kiểm tra
+  console.log("CLIENT ID:", process.env.SPOTIFY_CLIENT_ID);
+  console.log("REDIRECT URI:", process.env.SPOTIFY_REDIRECT_URI_NETLIFY);
+  
+  const {
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_REDIRECT_URI_NETLIFY
+  } = process.env;
+
+  // Nếu một trong các biến không tồn tại, trả về lỗi
+  if (!SPOTIFY_CLIENT_ID || !SPOTIFY_REDIRECT_URI_NETLIFY) {
+    return {
+      statusCode: 500,
+      body: "Lỗi: Biến môi trường SPOTIFY_CLIENT_ID hoặc SPOTIFY_REDIRECT_URI_NETLIFY không được thiết lập trên Netlify. Vui lòng kiểm tra và deploy lại."
+    }
+  }
+
   const scope = 'streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state';
 
   const authUrl = 'https://accounts.spotify.com/authorize?' +
@@ -18,7 +30,7 @@ exports.handler = async (event, context) => {
     });
 
   return {
-    statusCode: 302, // 302 là mã cho việc chuyển hướng tạm thời
+    statusCode: 302,
     headers: {
       Location: authUrl,
     },
